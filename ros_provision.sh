d16110df00270dbf75c9cd8b8324da132d519ed4
Chris McKinney <ntd5@tachibanatech.com>
2019-02-14 13:20:27 -0500

    Switched from sks keyserver to mit for ros

--- ros_provision.sh
+++ ros_provision.sh
@@ -8,7 +8,7 @@ apt-get install -y sudo
 # Install ROS
 echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" \
     > /etc/apt/sources.list.d/ros-latest.list
-apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key \
+apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-key \
     421C365BD9FF1F717815A3895523BAEEB01FA116
 apt-get update
 apt-get upgrade -y
