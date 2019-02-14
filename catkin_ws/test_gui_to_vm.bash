9721d3eb8d6405a6f58dd2ed59a3f6dede7310e0
Chris McKinney and Sam Dauchert <maestro@tachibanatech.com>
2019-02-14 15:49:18 -0500

    Attempt to make remote to vm work

--- catkin_ws/test_gui_to_vm.bash
+++ catkin_ws/test_gui_to_vm.bash
@@ -3,5 +3,7 @@ export ROS_HOSTNAME="$(curl https://ttech.click/tx2-ip.txt)"
 export ROS_MASTER_HOSTNAME="$1"
 export ROS_MASTER_URI="http://$ROS_MASTER_HOSTNAME:11311"
 export TURTLEBOT3_MODEL=waffle_pi
+source /opt/ros/kinetic/setup.bash
 source "$(dirname "$0")/source.bash"
-rosrun resilient_debug_gui gui.py
+shift
+"$@"
