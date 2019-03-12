8af6b300cc183c8d45f812826022c1820f316ba5
Chris McKinney and Sam Dauchert <maestro@tachibanatech.com>
2019-03-12 17:05:55 -0400

    ZED begin, landmark correction, script removal

--- catkin_ws/scripts/make.bash
+++ catkin_ws/scripts/make.bash
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 wd="$(pwd)"
 source source.bash
-cd ../xiaofeng-slam
+cd ../../xiaofeng-slam
 [ "$1" = '-c' ] && catkin clean --yes
 catkin build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug --
 . devel/setup.bash
--- catkin_ws/source.bash
+++
@@ -1,3 +0,0 @@
-source "$(dirname "$BASH_SOURCE")/../xiaofeng-slam/devel/setup.bash"
-source "$(dirname "$BASH_SOURCE")/devel/setup.bash"
-export TURTLEBOT3_MODEL=waffle_pi
