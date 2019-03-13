58cbc33e6aac4dec0f06adb084107f7c86eec277
Chris McKinney and Sam Dauchert <maestro@tachibanatech.com>
2019-03-13 17:07:40 -0400

    PF tracking for column landmarks in ls_slam

--- catkin_ws/scripts/make.bash
+++ catkin_ws/scripts/make.bash
@@ -4,8 +4,11 @@ wd="$(pwd)"
 source source.bash
 cd ../../xiaofeng-slam
 [ "$1" = '-c' ] && catkin clean --yes
-catkin build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug --
+cmake_args="--cmake-args \
+  -DCMAKE_C_COMPILER=gcc-8 -DCMAKE_CXX_COMPILER=g++-8
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug --"
+catkin build $cmake_args
 . devel/setup.bash
 cd "$wd"
 [ "$1" = '-c' ] && catkin clean --yes
-catkin build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -- "$@"
+catkin build $cmake_args "$@"
