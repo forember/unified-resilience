7921f32a8978731ffd0abe22a0f99473d1114ebe
Chris McKinney <ntd5@tachibanatech.com>
2019-03-18 13:02:40 -0400

    gcc 8

--- base_provision.sh
+++ base_provision.sh
@@ -11,6 +11,7 @@ yes | ./vpn_install.sh
 apt-get install -y libatk1.0-0 libgdk-pixbuf2.0-0 libgtk2.0-0
 apt-get install -y libpangox-1.0-0 libpangoxft-1.0-0
 apt-get install -y xterm virtualbox-guest-x11
+apt-get install -y gcc-8 g++-8
 apt-get install -y kde-plasma-desktop
 apt-get install -y libpam-kwallet5 signon-kwallet-extension
 apt-get install -y mosh nmap silversearcher-ag
