ed5b180199f210efa712708f02f48e2556b0fb9d
Chris McKinney <ntd5@tachibanatech.com>
2019-03-18 13:28:34 -0400

    gcc-8 locked up the VM

--- catkin_ws/scripts/make.bash
+++ catkin_ws/scripts/make.bash
@@ -4,9 +4,10 @@ wd="$(pwd)"
 source source.bash
 cd ../../xiaofeng-slam
 [ "$1" = '-c' ] && catkin clean --yes
-cmake_args="--cmake-args \
-  -DCMAKE_C_COMPILER=gcc-8 -DCMAKE_CXX_COMPILER=g++-8
-  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug --"
+cmake_args="--cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug"
+command -v gcc-8 && command -v g++-8 \
+    && cmake_args="$cmake_args -DCMAKE_C_COMPILER=gcc-8 -DCMAKE_CXX_COMPILER=g++-8"
+cmake_args="$cmake_args --"
 catkin build $cmake_args
 . devel/setup.bash
 cd "$wd"
